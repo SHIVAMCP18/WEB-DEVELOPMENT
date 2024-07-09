f38d8688cd851017cad23f5b04738c020b28bb05
SHIVAM CP <shivampatel180905@gmail.com>
2024-07-09 22:38:39 +0530

    Update Basic.js

--- JAVASCRIPT/Basic.js
+++ JAVASCRIPT/Basic.js
@@ -3,3 +3,14 @@ console.log(319);
 
 console.log(1 + 3);
 console.log(3 / 1);
+
+for(let i = 1 ; i <= 10 ; i++){
+    console.log(i);
+}
+
+let fullname = "shivam";
+
+for(let value of fullname){
+    console.log(value);
+}
+
